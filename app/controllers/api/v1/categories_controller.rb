3b04b6ab848569902d348430becc016ea0adb14a
suvankar <suvankar.17@gmail.com>
2016-01-20 02:02:12 +0530

    Added category api with product list belongs to that category

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::CategoriesController < Api::V1::BaseController
   def index
     @subcategories = Subcategory.all
   end
+
+  def show
+    @subcategory = Subcategory.find(params[:id])
+    products = @subcategory.products.all
+    @variants = []
+    
+    products.each do |product|
+      if product.has_variants?
+        product.variants.all.each { |variant| @variants << variant }
+      else
+        @variants << product
+      end
+    end
+    @variants
+  end
 end
